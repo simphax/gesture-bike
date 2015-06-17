35a357720d10ede8204a9923e17a0f52103d48b1
vvechev <velko.vechev@gmail.com>
2015-06-17 16:10:34 +0200

    Add NMEA Library for parsing GPS

--- OpenNI2Project.cpp
+++ OpenNI2Project.cpp
@@ -22,6 +22,8 @@
 #include "NiTE.h"
 using namespace openni;
 
+#include "nmea/nmea.h"
+
 
 // GLUT headers
 #include <GLUT/GLUT.h>
@@ -572,6 +574,74 @@ void gl_Setup(void) {
     glBindTexture(GL_TEXTURE_2D, 1);
 }
 
+void trace(const char *str, int str_size)
+{
+    printf("Trace: ");
+    write(1, str, str_size);
+    printf("\n");
+}
+void error(const char *str, int str_size)
+{
+    printf("Error: ");
+    write(1, str, str_size);
+    printf("\n");
+}
+
+void gpsTest()
+{
+    
+    nmeaINFO info;
+    nmeaPARSER parser;
+    FILE *file;
+    char buff[2048];
+    int size, it = 0;
+    nmeaPOS dpos;
+    
+    file = fopen("gpslog.txt", "rb");
+    
+    /*if(!file)
+    {
+        return -1;
+    }*/
+    
+    nmea_property()->trace_func = &trace;
+    nmea_property()->error_func = &error;
+    
+    nmea_zero_INFO(&info);
+    nmea_parser_init(&parser);
+    
+    /*
+     while(1)
+     {
+     */
+    
+    while(!feof(file))
+    {
+        size = (int)fread(&buff[0], 1, 100, file);
+        
+        nmea_parse(&parser, &buff[0], size, &info);
+        
+        nmea_info2pos(&info, &dpos);
+        
+        printf(
+               "%03d, Lat: %f, Lon: %f, Sig: %d, Fix: %d\n",
+               it++, dpos.lat, dpos.lon, info.sig, info.fix
+               );
+    }
+    
+    fseek(file, 0, SEEK_SET);
+    
+    /*
+     }
+     */
+    
+    nmea_parser_destroy(&parser);
+    fclose(file);
+    
+}
+
+
+
 
 int main(int argc, char* argv[])
 {
@@ -619,6 +689,8 @@ int main(int argc, char* argv[])
     hud = new HUD();
     
     printf("Starting OpenGL rendering process ...\r\n");
+    
+    gpsTest();
     glutMainLoop();
     
     return 0;
